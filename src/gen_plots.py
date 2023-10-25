cded78d4d339df72c968f566befaa5d1f9f34477
Abuzar Mahmood <abuzarmahmood@gmail.com>
2023-10-24 21:51:34 -0400

    Working version with modules

---
+++ src/gen_plots.py
@@ -0,0 +1,176 @@
+"""
+Utility for generating plots for specific examples 
+"""
+
+import numpy as np
+import pylab as plt
+import seaborn as sns
+import pymc as pm
+from scipy import stats
+import pandas as pd
+
+def gen_simple_plots(
+        years,
+        disaster_data,
+        disaster_model,
+        disaster_trace,
+        ):
+    with disaster_model:
+      disaster_ppc = pm.sample_posterior_predictive(disaster_trace)
+
+    ppc_values = disaster_ppc['posterior_predictive']['disasters']
+    mean_ppc, std_ppc = np.mean(ppc_values,axis=(0,1)),np.std(ppc_values,axis=(0,1))
+
+    disaster_switch_inferred = disaster_trace["posterior"]["switchpoint"]
+    mean_switch,std_switch = disaster_switch_inferred.mean(), disaster_switch_inferred.std()
+
+    fig, ax = plt.subplots(2,1,figsize=(10, 8))
+    ax[0].scatter(years, disaster_data, marker = ".", s = 200)
+    ax[0].set_ylabel("Number of accidents", fontsize=16)
+    ax[0].set_xlabel("Year", fontsize=16)
+
+    ax[0].vlines(mean_switch, disaster_data.min(), disaster_data.max(),
+               color="k", label = 'Mean switchpoint')
+
+    ax[0].vlines(disaster_switch_inferred.min(), disaster_data.min(), disaster_data.max(),
+               color="k", linestyles = 'dashed')
+    ax[0].vlines(disaster_switch_inferred.max(), disaster_data.min(), disaster_data.max(),
+               color="k", label = 'Switchpoint bounds', linestyles = 'dashed')
+    ax[0].axvspan(mean_switch-3*std_switch, mean_switch+3*std_switch, alpha = 0.5,
+                color='r', label = '+/- 3 std Switchpoint')
+    ax[0].plot(years, mean_ppc, label = 'Average disasters', c='red', lw=5)
+    ax[0].legend()
+
+    ax[1].scatter(years, disaster_data, marker = ".", s = 200)
+    ax[1].set_ylabel("Number of accidents", fontsize=16)
+    ax[1].set_xlabel("Year", fontsize=16)
+    ax[1].fill_between(years, mean_ppc-std_ppc, mean_ppc+std_ppc,
+                       label = '+/- 1 std mean rate', color='red', alpha = 0.5)
+    ax[1].vlines(disaster_switch_inferred.min(), disaster_data.min(), disaster_data.max(),
+               color="k", linestyles = 'dashed')
+    ax[1].vlines(disaster_switch_inferred.max(), disaster_data.min(), disaster_data.max(),
+               color="k", label = 'Switchpoint bounds', linestyles = 'dashed')
+    ax[1].legend()
+
+def gen_bernoulli_plots(
+    trace,
+    component_inds,
+    n_trials,
+    true_r):
+
+    tau_samples = trace['posterior']['tau']
+    int_tau = np.vectorize(int)(tau_samples)
+    mode_tau = stats.mode(int_tau,axis=0)[0][0]
+    hist_tau = np.array([np.histogram(trial, bins = np.arange(20))[0] for trial in int_tau.swapaxes(0,1)])
+    
+    w_samples = trace['posterior']['w'].values
+    w_samples_long = np.reshape(w_samples,(-1,w_samples.shape[-1]))
+    mean_w = np.mean(w_samples,axis=(0,1))
+    categorical_w = np.argmax(mean_w,axis=-1)
+    
+    cat_accuracy_list = [np.sum(categorical_w==component_inds),
+                          np.sum((1-categorical_w)==component_inds)]
+    model_ind = int(np.argmax(cat_accuracy_list))
+    cat_accuracy = cat_accuracy_list[model_ind]
+    
+    
+    if np.mean(categorical_w == component_inds) < 0.5:
+        plot_inferred_inds = 1-categorical_w
+    else:
+        plot_inferred_inds = categorical_w
+    
+    fig,ax = plt.subplots(1,3,figsize=(7,3))
+    ax[0].set_title('True rates with transitions')
+    ax[0].imshow(true_r,aspect='auto',origin='lower',vmin=0,vmax=1)
+    ax[1].plot(component_inds, np.arange(n_trials),'-o',label='Actual', alpha = 0.5)
+    ax[1].plot(plot_inferred_inds, np.arange(n_trials),'-x',label='Inferred', alpha = 0.5)
+    ax[1].set_title(f'Accuracy : {cat_accuracy}/{categorical_w.shape[0]}')
+    ax[1].legend()
+    for trial,val in enumerate(mode_tau):
+        ax[0].vlines(val, trial-0.5,trial+0.5, linewidth = 5, color='red')
+    ax[2].imshow(mean_w,aspect='auto',origin='lower');
+    ax[2].set_title('Mixture weights')
+    ax[1].set_ylabel('Trial #')
+    ax[1].set_xlabel('Mixture #')
+    ax[2].set_ylabel('Trial #')
+    ax[2].set_xlabel('Mixture #')
+    plt.tight_layout()
+
+def gen_dirichlet_plots(
+    dpp_trace,
+    n_trials,
+    true_r,
+    n_chains,
+    true_tau,
+    length,
+    n_states,
+    max_states,
+    ):
+
+    w_latent_samples = dpp_trace['posterior']['w_latent'].values
+    cat_w_latent_samples = np.concatenate(w_latent_samples)
+    sorted_lens = np.sort(cat_w_latent_samples,axis=-1)[:,::-1]
+
+    sorted_w_latent = np.stack(np.array_split(np.sort(w_latent_samples,axis=-1)[...,::-1],n_chains,axis=0))
+    sorted_w_latent = np.squeeze(sorted_w_latent)
+    mean_sorted = np.mean(sorted_w_latent, axis = 1)
+
+    all_state_edges = np.concatenate([[0],true_tau,[length]])
+    state_durations = np.abs(np.diff(all_state_edges))
+    sorted_state_durations = np.sort(state_durations / length)[::-1]
+    shortest_state = sorted_state_durations[-1]
+
+    inds = np.array(list(np.ndindex(mean_sorted.shape)))
+    state_frame = pd.DataFrame(
+                            dict(
+                                chains = inds[:,0],
+                                states = inds[:,1]+1,
+                                dur = mean_sorted.flatten()
+                            )
+                        )
+
+    fig,ax = plt.subplots(5,1, figsize = (7,15))
+
+    sns.stripplot(
+        data = state_frame,
+        x = 'states',
+        y = 'dur',
+        color = 'k',
+        ax = ax[0]
+    )
+    ax[0].plot(mean_sorted.T, alpha = 0.7, color = 'grey')
+
+    ax[0].axvline(n_states-1, zorder = -1, color = 'black', label = 'Actual')
+    ax[0].legend()
+    ax[0].axhline(shortest_state, color = 'red', linestyle = '--')
+    ax[0].text(0, shortest_state, 'Shortest state')
+    ax[0].axhline(0.01, color = 'red', linestyle = '--')
+    ax[0].text(0, 0.01, '0.01')
+    # ax[0].axhline(0.05, color = 'red', linestyle = '--')
+    # ax[0].text(0, 0.05, '0.05')
+
+    corrected_transitions = np.cumsum(sorted_w_latent,axis=-1)
+    tau_samples = dpp_trace['posterior']['tau'].values
+    ax[1].imshow(true_r,aspect='auto', interpolation = 'nearest')
+    # for x in tau_samples.T:
+    #     ax[2].hist(x, bins = np.arange(length), c = 'k')
+    ax[2].hist(tau_samples.flatten(), bins = np.arange(length), color = 'grey')
+    ax[2].sharex(ax[1])
+
+    im1 = ax[3].imshow(sorted_lens, interpolation='nearest', aspect= 'auto')
+    ax[3].set_title('Sorted latent_w')
+    fig.colorbar(im1, ax=ax[3])
+    plt.tight_layout()
+
+    max_state_per_chain = state_frame.loc[state_frame.dur > 0.01].groupby('chains').max()
+    max_state_counts = max_state_per_chain.groupby('states').count()
+    state_vec = np.arange(1,max_states+1)
+    counts = [max_state_counts.loc[x].values[0] if x in max_state_counts.index else 0 for x in state_vec ]
+    ax[4].axvline(n_states, zorder = 2, color = 'black', label = 'Actual')
+    ax[4].legend()
+    ax[4].bar(state_vec, counts)
+    ax[4].set_xlabel("States")
+    ax[4].set_ylabel('Count')
+
+    plt.tight_layout()
+
---
+++ src/get_data.py
@@ -0,0 +1,140 @@
+"""
+Utility for returning data for specific examples
+"""
+
+import numpy as np
+import pandas as pd
+
+
+
+def return_simple_data():
+    """
+    Returns data for the simple poisson changepoint example
+
+    Input:
+        None
+
+    Output:
+        counts: pd.Series of counts
+        years: np.array of years
+    """
+
+    disaster_data = pd.Series(
+            [4, 5, 4, 0, 1, 4, 3, 4, 0, 6, 3, 3, 4, 0, 2, 6,
+             3, 3, 5, 4, 5, 3, 1, 4, 4, 1, 5, 5, 3, 4, 2, 5,
+             2, 2, 3, 4, 2, 1, 3, np.nan, 2, 1, 1, 1, 1, 3, 0, 0,
+             1, 0, 1, 1, 0, 0, 3, 1, 0, 3, 2, 2, 0, 1, 1, 1,
+             0, 1, 0, 1, 0, 0, 0, 2, 1, 0, 0, 0, 1, 1, 0, 2,
+             3, 3, 1, np.nan, 2, 1, 1, 1, 1, 2, 4, 2, 0, 0, 1, 4,
+             0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1]
+            )
+    years = np.arange(1851, 1962)
+
+    na_bool = np.logical_not(np.isnan(disaster_data))
+    disaster_data = disaster_data.loc[na_bool]
+    years = years[na_bool]
+
+    return disaster_data, years
+
+def return_bernoulli_mixture_data(
+        length = 100,
+        n_states = 3,
+        n_trials = 30,
+        n_components = 2,
+        mixture_probs = None,
+        ):
+    """
+    Returns data for the bernoulli mixture example
+
+    Input:
+        length: length of data
+        n_states: number of states
+        n_trials: number of trials
+        n_components: number of mixutre components
+
+    Output:
+        true_lambda: true firing rate values
+        data_vec: spike_array
+        component_inds: indices of mixture components each trial is drawn from
+        idx: time indices
+    """
+    if mixture_probs is None: 
+        mixture_probs = np.random.random(n_components)
+        mixture_probs /= np.sum(mixture_probs)
+    else:
+        assert len(mixture_probs) == n_components, "mixture_probs must be of length n_components" 
+        assert np.sum(mixture_probs) == 1, "mixture_probs must sum to 1"
+
+    idx = np.arange(length)
+
+    # Generate transition times
+    true_tau = np.cumsum(np.random.random((n_trials,n_states)),axis=-1)
+    true_tau /= np.max(true_tau,axis=-1)[:,np.newaxis]
+    true_tau *= length
+    true_tau = np.vectorize(int)(true_tau)
+
+    # Generate state boundaries using transition times
+    state_inds = np.concatenate([np.zeros((n_trials,1)),true_tau],axis=-1)
+    state_inds = np.vectorize(int)(state_inds)
+    true_tau = true_tau[:-1]
+
+    # Generate emission values
+    true_lambda = np.random.random((n_components,n_states))
+
+    # Generate "trials" from n different mixture 
+    # components with uneven mixture probabilities
+    component_inds = np.random.choice(range(n_components), n_trials, p = [0.3,0.7])
+    true_r = np.zeros((n_trials,length))
+    for trial in range(n_trials):
+        for state in range(n_states):
+            true_r[trial, state_inds[trial,state]:state_inds[trial,state+1]] = \
+                    true_lambda[component_inds[trial],state]
+
+    # Final spiking data
+    data_vec = np.random.random(true_r.shape) < true_r
+
+    return true_r, data_vec, component_inds, idx
+
+def return_dirichlet_process_data(
+        length = 100,
+        n_states = 3,
+        n_neurons = 10,
+        seesaw_emissions = True,
+        ):
+    """
+    Returns data for the dirichlet process example
+
+    Input:
+        length: length of data
+        n_states: number of states
+        n_neurons: number of neurons
+
+    Output:
+        true_r: true firing rate values
+        true_tau: true state boundaries
+        true_lambda: true emission rates
+
+    """
+
+    true_tau = np.cumsum(np.random.random(n_states))
+    true_tau /= np.max(true_tau)
+    true_tau *= length
+    true_tau = np.vectorize(int)(true_tau)
+    state_inds = np.concatenate([np.zeros((1)),true_tau])
+    state_inds = np.vectorize(int)(state_inds)
+    true_tau = true_tau[:-1]
+
+    lambda_multipliers = np.random.random(n_states) * 10
+    if seesaw_emissions:
+        true_lambda = np.array(
+                [np.ones(n_neurons) * (i % 2) for i in range(n_states)]
+                ).T
+    else:
+        true_lambda = np.random.random((n_neurons,n_states))
+        true_lambda = true_lambda * lambda_multipliers[np.newaxis,:]
+
+    true_r = np.zeros((n_neurons,length))
+    for num, val in enumerate(true_lambda.T):
+        true_r[:,state_inds[num]:state_inds[num+1]] = val[:,np.newaxis]
+
+    return true_r, true_tau, true_lambda
